e3189fe5483c1ae38faef1176299c15e735023c4
carla stockalper <cstockalper@gmail.com>
2016-10-19 22:45:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+$(function() {
+
+// nav box shrink and grow functions
+let counter = 1;
+let changeBoxes = function() {
+  if (counter === 1) {
+    $('.boxes').animate({
+      height : '50px',
+      width : '50px',
+      borderRadius : '10px'
+    }, 2000);
+    $('h2').animate({
+      fontSize : '0.5em'
+      }, 2000);
+    counter = 2;
+  } else {
+    $('.boxes').animate({
+      height : '100px',
+      width : '100px',
+      borderRadius : '20px'
+    }, 2000);
+    $('h2').animate({
+      fontSize : '0.9em'
+      }, 2000);
+    counter = 1;
+  }
+};
+
+// nav label animation
+let appear = function() {
+  $('.labels').css("visibility", "visible");
+  console.log('label');
+};
+
+let disappear = function() {
+  $(this).css('visibility', 'hidden')
+};
+
+// event listeners
+$('.boxes').on('click', changeBoxes);
+$('#box1').click(appear);
+
+});
+
+
+
+// $('.hover').mouseover(function() {
+//   $('.text').css("visibility","visible");
+// });
+
+// $('.hover').mouseout(function() {
+//   $('.text').css("visibility","hidden");
+// });
