25d110f9c7542c3de250395562ab6702c2098097
carla stockalper <cstockalper@gmail.com>
2016-12-03 16:34:37 -0500

    rectangle page transition

--- script.js
+++ script.js
@@ -1,48 +1,96 @@
+console.log('script is connected')
+
 $(function() {
 
-// nav box shrink and grow functions
-let counter = 1;
-let changeBoxes = function() {
-  if (counter === 1) {
-    $('.boxes').animate({
-      height : '50px',
-      width : '50px',
-      borderRadius : '10px'
-    }, 2000);
-    $('h2').animate({
-      fontSize : '0.5em'
-      }, 2000);
-    counter = 2;
-  } else {
-    $('.boxes').animate({
-      height : '100px',
-      width : '100px',
-      borderRadius : '20px'
-    }, 2000);
-    $('h2').animate({
-      fontSize : '0.9em'
-      }, 2000);
-    counter = 1;
-  }
-};
+let activateWelcome = () => {
+  $('#welcome').animate({height: '100vh',width: '100vw'}, 500);
 
-// nav label animation
-// let appear = function() {
-//   $('.labels').css("visibility", "visible");
-// };
+  $('#about').animate({height: '0vh', width: '0vh'}, 500);
+  $('#projects').animate({height: '0vh', width: '0vh'}, 500);
+  $('#contact').animate({height: '0vh', width: '0vh'}, 500);
+  $('#welcomeContent').animate({opacity: '1'}, 1000);
+  $('.title').css({'visibility': 'hidden'});
+}
 
-// event listeners
-$('.boxes').on('click', changeBoxes);
-$('#box1').click(appear);
+let activateAbout = () => {
+  $('#about').animate({height: '100vh',width: '100vw'}, 500);
+  
+  $('#welcome').animate({height: '0vh', width: '0vh'}, 500);
+  $('#projects').animate({height: '0vh', width: '0vh'}, 500);
+  $('#contact').animate({height: '0vh', width: '0vh'}, 500);
+  $('#aboutContent').animate({opacity: '1'}, 1000);
+  $('.title').css({'visibility': 'hidden'});
+}
+
+let activateProjects = (e) => {
+  $(e.target).animate({height: '100vh',width: '100vw'}, 500);
+  
+  $('#welcome').animate({height: '0vh', width: '0vh'}, 500);
+  $('#about').animate({height: '0vh', width: '0vh'}, 500);
+  $('#contact').animate({height: '0vh', width: '0vh'}, 500);
+  $('#projectsContent').animate({opacity: '1'}, 1000);
+  $('.title').css({'visibility': 'hidden'});
+}
+
+let activateContact = (e) => {
+  $(e.target).animate({height: '100vh',width: '100vw'}, 500);
+  
+  $('#welcome').animate({height: '0vh', width: '0vh'}, 500);
+  $('#about').animate({height: '0vh', width: '0vh'}, 500);
+  $('#projects').animate({height: '0vh', width: '0vh'}, 500);
+  $('#contactContent').animate({opacity: '1'}, 1000);
+  $('.title').css({'visibility': 'hidden'});
+}
+
+let back = () => {
+
+  $('.rectangleContent').animate({opacity: '0'}, 500);
+  $('.title').css({'visibility': 'visible'});
+}
+
+// Event Listneners
+$('#welcome').on('click', activateWelcome)
+$('#about').on('click', activateAbout)
+$('#projects').on('click', activateProjects)
+$('#contact').on('click', activateContact)
+$('.backButton').on('click', back)
 
 });
 
 
 
-// $('.hover').mouseover(function() {
-//   $('.text').css("visibility","visible");
-// });
 
-// $('.hover').mouseout(function() {
-//   $('.text').css("visibility","hidden");
-// });
+
+// let openPage = (e) => {
+//    // $(e.target).animate({height: '100vh',width: '100vw'}, 500);
+//   // console.log(e.target);
+//   // let otherBoxes = $('.rectangle') !== e.target
+//   // console.log(otherBoxes);
+
+//   //let selected = e.target;
+//   let rectangle = $('.rectangle');
+//   //console.log(rectangle);
+
+//   rectangle.each ((e) => {
+//     let section = $(this);
+//     section.on('click', () => {
+//     section.css({'backgroundColor' : "black"})
+    
+
+//     })
+//   })
+// }
+  
+//   // if (rectangle == selected) {
+//   //   selected.addClass('fullView');
+//   // };
+
+//   //     {height: '100vh',width: '100vw'}, 500);
+//   // } else {
+//   //   $('.rectangle').css({"height" : '0vh', "width" : '0vh'});
+//   // };
+
+  
+// //   $('#aboutContent').animate({opacity: '1'}, 1000);
+// //   $('.title').css({'visibility': 'hidden'});
+// // }
